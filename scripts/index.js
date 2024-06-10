5dc476508f83782c2c693e5dfd51bc1835059d01
Rauf Kamachkin <rauf.kamachkin@hotmail.com>
2024-06-10 16:16:46 +0300

    version 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, Delete) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+//Прилинковываем данные с InitialCards
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    deleteButton.addEventListener('click', () => {
+        Delete(cardElement);
+    });
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function DeleteCard(cardElement) {
+    cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, DeleteCard);
+    placesList.append(cardElement);
+});
